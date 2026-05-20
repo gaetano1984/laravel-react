c95cdf749853608e7b44d5e34f9ebfdc522d4db6
gaetano1984 <gaecamp@gmail.com>
2026-05-20 15:11:59 +0200

    intertia
    inertia

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <!-- @vite(['resources/css/app.css', 'resources/app/app.js']) -->
+        @viteReactRefresh
+        @vite('resources/js/app.jsx')
+    </head>
+    <body class="bg-gray-100">
+        <div id="app" class="container mx-auto px-10 items-center" />
+    </body>
+</html>
