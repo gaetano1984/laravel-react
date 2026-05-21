ce677acab9679c7fcba7f9590dc6a6dd94695261
Gaetano Campanella <gaetano.campanella@novatag.it>
2026-05-21 15:59:41 +0200

    card ristoranti

--- resources/js/Pages/Restaurants/List.jsx
+++ resources/js/Pages/Restaurants/List.jsx
@@ -19,25 +19,37 @@ export default function List(){
                     <div className="bg-blue-300 p-2 text-center font-semibold">
                         Lista dei ristoranti
                     </div>
-                    <div className="grid grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4 p-2">
+                    <div className="grid grid-cols-2 gap-4 p-2">
                         {restaurants?.length === 0 ? (
                             <div>
                                 Attenzione: nessun ristorante disponibile al momento
                             </div>                            
                         ) : (
                             restaurants.map((restaurant) => (
-                                <div className="border border-gray-300 rounded-lg" key={restaurant.id}>
-                                    <div className="bg-blue-300 p-2 text-center font-semibold">
-                                        {restaurant.name}
+                                <div className="bg-white border border-gray-300 rounded-lg p-2">
+                                    <div className="flex h-full items-stretch">
+                                        <div className="w-1/2 shrink-0">
+                                            <img className="w-full h-full max-h-45 object-cover rounded-lg" src="/images/img-restaurant.jpeg" />
+                                        </div>
+                                        <div className="flex-1 p-2 flex flex-col justify-between pl-5">
+                                            <div className="font-semibold text-lg">
+                                                {restaurant.name}
+                                            </div>               
+                                            <div className="font-extralight pt-3 pb-3">
+                                                <span className="bg-emerald-50 text-emerald-700  boder border-black rounded-lg font-medium p-2">
+                                                    {restaurant.type}
+                                                </span>
+                                            </div>                             
+                                            <div className="text-sm font-light">
+                                                {restaurant.address}, {restaurant.city} - {restaurant.CAP}
+                                            </div>
+                                            <div className="center mt-0">
+                                                <Button className="bg-blue-400 text-white p-2 rounded-4xl w-32 mt-4" text="Prenota" onClick={() => alert("Funzionalità in sviluppo")} />
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="p-2">
-                                        <div>Indirizzo: {restaurant.address}, {restaurant.city}</div>
-                                    </div>             
-                                    <div className="p-4 text-center">
-                                        <Button className="bg-blue-400 text-white p-4 pt-2 pb-2 rounded-4xl w-32 m-auto" text="Vai al menu" onClick={() => alert(`Vai al ristorante ${restaurant.name}`)}/>
-                                    </div>
-                                </div>                            
-                            ))
+                                </div>
+                            ))                            
                         )}
                     </div>                    
                 </div>
