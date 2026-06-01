e549edd86800cac242a5640da31fe9e33c5fa5e9
gaetano campanella <gaecamp@gmail.com>
2026-06-02 00:06:12 +0200

    dopo la creazione dei piatti, synco su strapi

--- app/Filament/Resources/DishResource/Pages/CreateDish.php
+++ app/Filament/Resources/DishResource/Pages/CreateDish.php
@@ -5,8 +5,27 @@ namespace App\Filament\Resources\DishResource\Pages;
 use App\Filament\Resources\DishResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Stripe\StripeClient;
+use App\Models\Dish;
 
 class CreateDish extends CreateRecord
 {
     protected static string $resource = DishResource::class;
+
+    public function afterCreate(){
+        $stripe = new StripeClient(env('STRIPE_SECRET'));
+        $product = $stripe->products->create([
+            'name' => $this->record->name,
+            'default_price_data' => [
+                'unit_amount' => $this->record->price,
+                'currency' => 'eur',
+            ],
+        ]);
+
+        $dish = Dish::find($this->record->id);
+        $dish->stripe_id = $product['id'];
+        $dish->save();
+
+        \Log::info(print_r($product, 1));
+    }
 }
