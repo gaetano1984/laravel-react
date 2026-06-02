1c8927b2b1693b37af1aac5d73ba455dfbefb6ac
gaetano campanella <gaecamp@gmail.com>
2026-06-02 10:58:56 +0200

    aggiornamento prezzi

--- app/Filament/Resources/DishResource/Pages/EditDish.php
+++ app/Filament/Resources/DishResource/Pages/EditDish.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\DishResource\Pages;
 use App\Filament\Resources\DishResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Stripe\StripeClient;
+use App\Models\Dish;
 
 class EditDish extends EditRecord
 {
@@ -15,5 +17,20 @@ class EditDish extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function afterSave(){
+        $stripe = new StripeClient(env('STRIPE_SECRET'));
+        $price = $stripe->prices->create([
+            'currency' => 'eur'
+            ,'unit_amount' => intval($this->record->price)
+            ,'product' => $this->record->stripe_id
+        ]);
+        $product = $stripe->products->update($this->record->stripe_id, [
+            'name' => $this->record->name,
+            'default_price' => $price['id']
+        ]);
+        $this->record->price_id = $price['id'];
+        $this->record->save();
     }
 }
