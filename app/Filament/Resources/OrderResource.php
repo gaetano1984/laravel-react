224ded2a1690ee443ce836711408a547542f42c6
gaetano campanella <gaecamp@gmail.com>
2026-06-05 10:26:07 +0200

    riepilogo ordine nel backend

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -17,7 +17,11 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\KeyValue;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\PLaceholder;
+use Filament\Forms\Components\Fieldset;
 use App\Models\Dish;
+use App\Models\User;
+use Illuminate\Support\HtmlString;
 
 class OrderResource extends Resource
 {
@@ -30,29 +34,25 @@ class OrderResource extends Resource
         return $form
             ->schema([
                 //
-                Card::make([
-                    Repeater::make('order')
-                    ->schema(function($record){
-                        $order = $record->order;
-                        $inputs = [];
-                        foreach($order as $row_order){
-                            $inputs['name'] = TextInput::make('name')->formatStateUsing(function() use($row_order){
-                                $dish = Dish::where('price_id', $row_order['price'])->first();
-                                return $dish['name'];
-                            }); 
-                            $inputs['price'] = TextInput::make('price')->formatStateUsing(function() use($row_order){
-                                $dish = Dish::where('price_id', $row_order['price'])->first();
-                                return $dish['price']."€";
-                            });
-                            $inputs['quantity'] = TextInput::make('quantity')->formatStateUsing(function() use($row_order){
-                                return $row_order['quantity'];
-                            });
-                        }
-                        return $inputs;
-                    })
-                    ->label('Dettagli ordine')  
+                Fieldset::make('dettagli ordine')->schema([
+                    Card::make([
+                        Placeholder::make('order')
+                        //->label('dettagli carrello')
+                        ->content(function($record){
+                            $order = json_decode($record->order, TRUE);
+                            $totale = 0;
+                            $table = array_map(function($a) use (&$totale){
+                                $dish = Dish::where('price_id', $a['price'])->first();
+                                $totale += $dish['price']*$a['quantity'];
+                                return "<li>".$dish['name']." -  ".$a['quantity']." x ".$dish['price']."€</li>";
+                            }, $order);
+                            $table = "<ul>".implode("", $table)."</ul>";
+                            $table .= "Totale: ".$totale."€";   
+                            return new HtmlString($table);
+                        })                        
+                    ])
+                    
                 ])
-                
             ]);
     }
 
@@ -61,7 +61,10 @@ class OrderResource extends Resource
         return $table
             ->columns([
                 //
-                TextColumn::make('user.email')->label('Cliente'),
+                TextColumn::make('user.email')->label('Cliente')->formatStateUsing(function($record){
+                    $u = User::find($record['user_id'])->first();
+                    return $u->name." ".$u->last_name;
+                }),
                 TextColumn::make('restaurant.name')->label('Ristorante'),
                 TextColumn::make('status')->label('Stato')
             ])
